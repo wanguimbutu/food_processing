648ea47aaf273230e39cd210f5ddde6ca75e387a
wanguimbutu <wanguimbutu@gmail.com>
2025-03-10 12:18:39 +0300

    adds meal tags

--- food_processing/food_processing/doctype/recipe/recipe.js
+++ food_processing/food_processing/doctype/recipe/recipe.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2025, wanguimbutu and contributors
 // For license information, please see license.txt
+frappe.ui.form.on('Recipe', {
+    after_save: function(frm) {
+        let tags = [];
 
-// frappe.ui.form.on("Recipe", {
-// 	refresh(frm) {
+        if (frm.doc.dairy) tags.push("Dairy");
+        if (frm.doc.wheatgluten) tags.push("Wheat");
+        if (frm.doc.nuts) tags.push("Nuts");
+        if (frm.doc.eggs) tags.push("Eggs");
+        if(frm.doc.fish) tags.push("Fish");
+        if(frm.doc.beef) tags.push("Beef");
+        if(frm.doc.poultry) tags.push("Poultry");
+        if(frm.doc.shellfish) tags.push("Shellfish");
+        if(frm.doc.soy) tags.push("Soy");
 
-// 	},
-// });
+        if (tags.length > 0) {
+            frappe.call({
+                method: "food_processing.food_processing.doctype.recipe.recipe.add_recipe_tags",
+                args: {
+                    recipe_name: frm.doc.name, 
+                    tags: tags.join(", ")       
+                },
+                callback: function(r) {
+                    if (!r.exc) {
+                        console.log("Tags successfully added:", tags);
+                    } else {
+                        console.error("Error adding tags:", r.exc);
+                    }
+                }
+            });
+        }
+    }
+});
--- food_processing/food_processing/doctype/recipe/recipe.py
+++ food_processing/food_processing/doctype/recipe/recipe.py
@@ -1,9 +1,33 @@
 # Copyright (c) 2025, wanguimbutu and contributors
 # For license information, please see license.txt
-
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class Recipe(Document):
 	pass
+
+@frappe.whitelist()
+def add_recipe_tags(recipe_name, tags):
+
+    if not recipe_name or not tags:
+        frappe.throw("Missing required parameters: 'recipe_name' or 'tags'")
+
+    tags = [tag.strip() for tag in tags.split(",") if tag.strip()]
+
+    for tag in tags:
+        if not frappe.db.exists("Tag", tag):
+            tag_doc = frappe.get_doc({"doctype": "Tag", "name": tag})
+            tag_doc.insert(ignore_permissions=True)
+
+        
+        if not frappe.db.exists("Tag Link", {"document_type": "Recipe", "document_name": recipe_name, "tag": tag}):
+            tag_link = frappe.get_doc({
+                "doctype": "Tag Link",
+                "tag": tag,
+                "document_type": "Recipe",
+                "document_name": recipe_name
+            })
+            tag_link.insert(ignore_permissions=True)
+
+    return {"status": "success", "tags_added": tags}
