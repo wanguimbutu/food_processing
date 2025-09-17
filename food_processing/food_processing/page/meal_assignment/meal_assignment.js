4678bfd706f84ca5a6d983c10c444e77eddf42ef
wanguimbutu <wanguimbutu@gmail.com>
2025-09-17 14:10:15 +0300

    revert task key but add task name

--- food_processing/food_processing/page/meal_assignment/meal_assignment.js
+++ food_processing/food_processing/page/meal_assignment/meal_assignment.js
@@ -384,7 +384,7 @@ frappe.pages['meal-assignment'].on_page_load = function(wrapper) {
                     const start = frappe.datetime.str_to_obj(task.exp_start_date);
                     const end = frappe.datetime.str_to_obj(task.exp_end_date);
                     
-                    const taskKey = task.name;
+                    const taskKey = `${customer}_${task.name}_${task.exp_start_date}_${task.exp_end_date}`;
                     getColorForCustomer(customer, assignedColors);
 
                     if (!customerMap[taskKey]) {
