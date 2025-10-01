42ee6ca9382d77a7d54f0c26a716589e0d885e22
wanguimbutu <wanguimbutu@gmail.com>
2025-10-01 15:45:04 +0300

    add material request button

--- food_processing/food_processing/report/daily_meal_issue/daily_meal_issue.js
+++ food_processing/food_processing/report/daily_meal_issue/daily_meal_issue.js
@@ -1,5 +1,5 @@
-frappe.query_reports["Meal Plan Quantities Report"] = {
-     "filters": [
+frappe.query_reports["Daily Meal Issue"] = {
+    "filters": [
         {
             "fieldname": "from_date",
             "label": "From Date",
@@ -13,4 +13,40 @@ frappe.query_reports["Meal Plan Quantities Report"] = {
             "reqd": 1
         }
     ],
-}
+
+    onload: function(report) {
+        report.page.add_inner_button(__('Create Material Request'), function() {
+            let d = new frappe.ui.Dialog({
+                title: 'Select Warehouse',
+                fields: [
+                    {
+                        label: 'Required By Warehouse',
+                        fieldname: 'warehouse',
+                        fieldtype: 'Link',
+                        options: 'Warehouse',
+                        reqd: 1
+                    }
+                ],
+                primary_action_label: 'Create',
+                primary_action(values) {
+                    let filters = report.get_values();
+                    filters.warehouse = values.warehouse;
+
+                    frappe.call({
+                        method: "food_processing.food_processing.report.daily_meal_issue.daily_meal_issue.make_material_request",
+                        args: { filters },
+                        callback: function(r) {
+                            if (r.message) {
+                                frappe.msgprint(__('Material Request {0} created', [r.message]));
+                                frappe.set_route("Form", "Material Request", r.message);
+                            }
+                        }
+                    });
+                    d.hide();
+                }
+            });
+
+            d.show();
+        });
+    }
+};
